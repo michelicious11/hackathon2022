56c7b91b9a289081517e5ed9b93707481c32d95f
Jason Nickopoulos <jnickopoulos@live.com>
2022-01-23 12:19:29 -0500

    First commit (camera)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:snapcity/main.dart';
+import 'package:hack_a_city/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
