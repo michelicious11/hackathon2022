a406af1a2a88b5f65e2e65596a29c6a68854dffc
Jason Nickopoulos <jnickopoulos@live.com>
2022-01-23 13:24:29 -0500

    Second commit (to merge with mich)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:hack_a_city/main.dart';
+import 'package:snapcity/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
