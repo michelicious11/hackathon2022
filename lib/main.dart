2c911043104e20a1f6103b5f247711e6a2e82c03
Jason Nickopoulos <jnickopoulos@live.com>
2022-01-23 16:12:04 -0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'dart:io';
-//import 'package:flutter/widgets.dart';
 
-import 'SnapBtn.dart';
 
-void main() => runApp(MyApp());
-
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() {
-    //TODO: implement createState
-    return _MyAppState();
-  }
+void main() {
+  runApp(const MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  var _test = 0;
-
-  void _takePicture() {
-    setState(() {
-      _test++;
-    });
-    print("test");
-  }
-=========
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
->>>>>>>>> Temporary merge branch 2
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("SnapCity"),
-        ),
-      body: SnapBtn(_takePicture),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
